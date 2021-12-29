4d19cf41b1f28dba6815ece32c1b2cb06fffc69b
loshuevostv <lore97drk@icloud.com>
2021-12-29 10:17:03 +0100

    update stinger

--- stinger/renderer.py
+++
@@ -1,6 +0,0 @@
-from html2image import Html2Image
-
-for i in range(1,121):
-    delay = int(i*200/12)
-    hti = Html2Image(output_path='frames/', custom_flags=['--virtual-time-budget='+ str(delay), '--hide-scrollbars'])
-    hti.screenshot(html_file='stinger.html', size=(1920, 1080), save_as='frame-'+ str(i) +'.png')
